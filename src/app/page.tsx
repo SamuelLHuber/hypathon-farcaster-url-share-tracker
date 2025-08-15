de6ec8a885f2d266777b63dabddebdd024383895
Samuel Huber <samuel@dtech.vision>
2025-08-15 15:38:56 +0200

    add dynamic preview image to page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
 import { Metadata } from "next";
 import App from "./app";
-import { APP_NAME, APP_DESCRIPTION, APP_OG_IMAGE_URL } from "~/lib/constants";
+import { APP_URL, APP_NAME, APP_DESCRIPTION, APP_OG_IMAGE_URL } from "~/lib/constants";
 import { getFrameEmbedMetadata } from "~/lib/utils";
 
 export const revalidate = 300;
 
-export async function generateMetadata(): Promise<Metadata> {
+export async function generateMetadata({ searchParams }: { searchParams: Promise<{ url?: string }> }): Promise<Metadata> {
+  const { url } = await searchParams;
+  const ogImageUrl = url 
+    ? `${APP_URL}/api/opengraph-image?url=${encodeURIComponent(url)}`
+    : APP_OG_IMAGE_URL;
+
   return {
     title: APP_NAME,
     openGraph: {
       title: APP_NAME,
       description: APP_DESCRIPTION,
-      images: [APP_OG_IMAGE_URL],
+      images: [ogImageUrl],
     },
     other: {
-      "fc:frame": JSON.stringify(getFrameEmbedMetadata()),
-      "fc:miniapp": JSON.stringify(getFrameEmbedMetadata()),
+      "fc:frame": JSON.stringify(getFrameEmbedMetadata(ogImageUrl)),
+      "fc:miniapp": JSON.stringify(getFrameEmbedMetadata(ogImageUrl)),
     },
   };
 }
