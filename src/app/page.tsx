4bfff861ac0e85d34c124bede19fe9f2dc2e9132
Samuel Huber <samuel@dtech.vision>
2025-08-15 15:13:17 +0200

    fix APPURL, fully working state

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,6 +15,7 @@ export async function generateMetadata(): Promise<Metadata> {
     },
     other: {
       "fc:frame": JSON.stringify(getFrameEmbedMetadata()),
+      "fc:miniapp": JSON.stringify(getFrameEmbedMetadata()),
     },
   };
 }
